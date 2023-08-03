33acc54538755c7ecf0d457fff37958ee91c4295
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-08-03 23:27:44 +0400

    08/03

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import React, { useState, useEffect, useRef } from 'react';
+import Button from './src/components/button';
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null); 
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    })
+  }, [])
+  
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        console.log(data);
+        setImage(data.uri);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!image ?
+        <Camera
+          style={styles.camera}
+          type={type}
+          flashMode={flash}
+          ref={cameraRef}
+        >
+          <Text>Hello</Text>
+        </Camera>
+        :
+        <Image source={{ uri: image }} style={styles.camera}/>
+      }
+      <View>
+        <Button title={'Take a picture'} icon={'camera'} onPress={takePicture}/>
+      </View>
+
     </View>
   );
 }
@@ -14,7 +61,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingBottom: 20
   },
+  camera: {
+    flex: 1,
+    borderBottomEndRadius: 20,
+    
+  }
 });
