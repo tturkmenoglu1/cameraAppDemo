714df05b9ddf268537a9446bb6ec21f6d5e18c74
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-10-24 21:44:40 +0400

    09/2

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react'
-import Camera from './src/components/CameraComp';
+import CameraComp from './src/components/CameraComp';
+import { TouchableOpacity } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [isCameraOpen, setCameraOpen] = useState(false);
+
+  const handleCameraPress = () => {
+    setCameraOpen(true);
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name='Camera'
-          component={Camera}
-          options={{title:"Go to Camera"}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      {/* Ana ekran içeriği */}
+      {isCameraOpen ? (
+        <CameraComp />
+      ) : (
+        <>
+          {/* Kamera simgesi */}
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              bottom: 20,
+              left: '50%',
+              transform: [{ translateX: -25 }],
+            }}
+            onPress={setCameraOpen(true)}
+          >
+            <Icon name="camera" size={50} color="black" />
+          </TouchableOpacity>
+        </>
+      )}
+    </View>
   )
 }
 
